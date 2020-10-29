3891967d5de33b227a58dc41d0e64a777b840d05
Igor <igor.rangel@ciadocredito.net.br>
2020-10-29 12:34:54 -0300

    commit

--- src/__tests__/lab.test.ts
+++ src/__tests__/lab.test.ts
@@ -1,29 +1,4 @@
-// import { koala } from "../index";
-//
-// jest.setTimeout(10000000);
-// test('Lab', async () => {
-// 	const text = `Recusada
-//
-//
-// Recusa automatica - 28/10/2020 12:10:20
-//
-//
-// - POLÍTICA DE CRÉDITO DEFINITIVA`;
-// 	console.log(koala(text).string()
-// 	                       .nbl2br()
-// 	                       .normalize()
-// 	                       .split('<br/>')
-// 	                       .map((value, index) => {
-// 	                       	if (index > 0) {
-// 		                        if (value.indexOf('- ') === 0) {
-// 			                        value = value.replace('- ', '');
-// 		                        }
-// 		                        return value;
-// 	                        }
-// 	                       	return '';
-// 	                       })
-// 	                       .clearEmptyValues()
-// 	                       .toString(' - ')
-// 	                       .getValue());
-// 	expect(true).toBe(true);
-// });
+jest.setTimeout(10000000);
+test('Lab', async () => {
+	expect(true).toBe(true);
+});
