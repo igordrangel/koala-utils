d3bc25272f0040676d32c4029bf90e0db0c99d9e
Igor Rangel <igordrangel@gmail.com>
2021-03-26 11:13:19 -0300

    commit

--- src/utils/KlString.ts
+++ src/utils/KlString.ts
@@ -103,8 +103,10 @@ export class KlString extends KlAbstract<string> {
   }
 
   public maskCnpj() {
-    this.value = this.leftPad(this.value.replace(/\D/g, ''), 14)
-      .replace(/^(\d{2})(\d{3})?(\d{3})?(\d{4})?(\d{2})?/, "$1.$2.$3/$4-$5");
+    this.value = this.leftPad(this.value.replace(/\D/g, ''), 14).replace(
+      /^(\d{2})(\d{3})?(\d{3})?(\d{4})?(\d{2})?/,
+      '$1.$2.$3/$4-$5',
+    );
 
     return this;
   }
