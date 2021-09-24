8239bd9501aa924ac008ae10e0fe2360f25b5acd
Igor Rangel <igordrangel@gmail.com>
2021-09-24 20:21:26 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { KlUtils } from './utils/KlUtils';
 
+export { format, isHoliday, add, sub, diff } from './operators/date';
+export { delay } from './operators/delay';
+export { maskCoin } from './operators/number';
+export { maskCpf, maskCnpj, randomString, clear, unmaskCoin, toCamelCase, nbl2br } from './operators/string';
+
+export { KlDateDay } from './enums/KlDateDay';
+
 export function koala(value: any) {
   return new KlUtils(value);
 }
