70092651528c8369efb49d670d4fd9c2081a4a7a
Igor Rangel <igordrangel@gmail.com>
2021-09-24 18:54:23 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { KlUtils } from './utils/KlUtils';
 
+export * from './operators/string';
+export * from './operators/number';
+export * from './operators/date';
+
 export function koala(value: any) {
   return new KlUtils(value);
 }
