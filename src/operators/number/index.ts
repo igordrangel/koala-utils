c1d9234991986afbc6628c61d058e0a17ba0a6e4
Igor Rangel <igordrangel@gmail.com>
2021-09-29 16:59:12 -0300

    commit

--- src/operators/number/index.ts
+++ src/operators/number/index.ts
@@ -2,15 +2,15 @@ import { koala } from '../../index';
 
 export function maskCoin(
   value: number,
-  options: {
-    prefix: string;
-    thousands: string;
-    decimal: string;
-    decimalCount: number;
+  options?: {
+    prefix?: string;
+    thousands?: string;
+    decimal?: string;
+    decimalCount?: number;
   },
 ) {
   return koala(value)
     .number()
-    .maskCoin(options.prefix, options.thousands, options.decimal, options.decimalCount)
+    .maskCoin(options?.prefix, options?.thousands, options?.decimal, options?.decimalCount)
     .getValue();
 }
